491ed5797ac8eaca832fc22705667392bf05370f
lucasbarreto <lucasbrittobr@gmail.com>
2022-04-17 19:35:43 -0300

    Kata 1.1

--- Katas/kata2.js
+++ Katas/kata2.js
@@ -1,9 +1,5 @@
 /* Given an integral number, determine if it's a square number: In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. The tests will always use some integral number, so don't worry about that in dynamic typed languages. */
 
-var isSquare = function(n){
-    if(n === 0){
-    return true
-    } else {
-    return n > 0 && Math.sqrt(n) % 1 === 0;
-    }
+function isSquare(n) {
+    return Math.sqrt(n) % 1 === 0;
   }
